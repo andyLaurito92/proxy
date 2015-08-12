551bd3e6389e56c618476ec028282278b9bc6a8a
andyLaurito92 <andy.laurito@gmail.com>
2015-08-11 23:30:06 -0300

    Estructura de la api rest de estadisticas utilizando cakephp3.0.

---
+++ apiRestEstadisticas/ApiEstadisticas/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         0.10.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+ini_set("display_errors", 1);
+ini_set("track_errors", 1);
+ini_set("html_errors", 1);
+error_reporting(E_ALL);
+
+require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
